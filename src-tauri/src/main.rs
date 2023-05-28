2352423120418310bd68a2b939ae8a437d12b3d8
Umatriz <artsharyi@gmail.com>
2023-05-28 09:56:12 +0300

    Start project

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,11 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+pub mod commands;
+pub mod utils;
+
+fn main() {
+  tauri::Builder::default()
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
