dd9d925ef30a2ab10247b22b440153697f6fd521
Umatriz <artsharyi@gmail.com>
2023-06-07 13:55:28 +0300

    Client skeleton

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+  import StartForm from './components/StartForm.vue';
+</script>
+
+<template>
+  <p>test</p>
+  <StartForm />
+</template>
---
+++ src/components/StartForm.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue';
+
+const username = ref("")
+const version = ref("")
+const load = ref(false)
+
+const download = () => {
+
+}
+
+const play = () => {
+  
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>Nomi</h1>
+    <input name="username" type="text" class="text_input" v-model.trim="username">
+    {{ username }}
+    <input name="version" type="text" class="text_input" v-model.trim="version">
+    <button class="button" @click="download">Download</button>
+    <button class="button" @click="play">Play</button>
+  </div>
+</template>
