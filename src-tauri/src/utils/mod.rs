77be1a04f21b3579b485e076e40bb3f136f78434
Umatriz <artsharyi@gmail.com>
2023-06-08 20:30:59 +0300

    better write_config

--- src-tauri/src/utils/mod.rs
+++ src-tauri/src/utils/mod.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -53,14 +55,28 @@ impl Config {
     }
   }
 
-  pub fn write_config(&self) {
-    let cdir = std::env::current_dir().unwrap();
-    if cdir.join("config.yaml").exists() {
-      todo!()
-    } else {
-      let file = std::fs::File::create(cdir.join("config.yaml")).unwrap();
-      let _ = serde_yaml::to_writer(&file, &self);
+  pub fn does_exist(&self) -> (bool, Option<PathBuf>) {
+    let config = std::env::current_dir().unwrap().join("config.yaml");
+    if config.exists() {
+      return (true, Some(config))
     }
+    return (false, None);
+  }
+
+  pub fn write_config(&self) -> Result<(), serde_yaml::Error> {
+    if self.does_exist().0 {
+      print!("Config already exist");
+      Ok(())
+    } else {
+      let file = std::fs::File::create(self.does_exist().1.unwrap()).unwrap();
+      let _ = serde_yaml::to_writer(&file, &self);
+      println!("created config");
+      Ok(())
+    }
+  }
+
+  pub fn read_config(&self) {
+    todo!()
   }
 
   pub fn add_profile(&mut self, profile: Profile) {
