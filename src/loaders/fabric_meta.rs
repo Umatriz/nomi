98685e34cd20f5beab663aaaca09a191dacffd75
Umatriz <artsharyi@gmail.com>
2023-07-08 09:35:10 +0300

    fix: `Meta` fix

--- src/loaders/fabric_meta.rs
+++ src/loaders/fabric_meta.rs
@@ -1,6 +1,9 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
+pub struct Meta(Vec<VersionLoader>);
+
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct VersionLoader {
     pub loader: Loader,
@@ -12,7 +15,7 @@ pub struct VersionLoader {
 #[serde(rename_all = "camelCase")]
 pub struct Loader {
     pub separator: String,
-    pub build: i8,
+    pub build: i32,
     pub maven: String,
     pub version: String,
     pub stable: bool,
@@ -29,7 +32,7 @@ pub struct Intermediary {
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct LauncherMeta {
-    pub version: i8,
+    pub version: i32,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
