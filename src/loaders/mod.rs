ffd1b8c8db8abbe8612aecbdfb7aa8c4c542c7b8
Umatriz <artsharyi@gmail.com>
2023-07-08 19:01:49 +0300

    `Loader` trait update

--- src/loaders/mod.rs
+++ src/loaders/mod.rs
@@ -8,12 +8,30 @@ pub mod maven;
 pub const QUILT_META: &str = "https://meta.quiltmc.org/";
 pub const QUILT_MAVEN: &str = "https://maven.quiltmc.org/";
 
-pub const FABRIC_META: &str = "https://meta.fabricmc.net/";
+pub const FABRIC_META: &str = "https://meta.fabricmc.net/v2";
 pub const FABRIC_MAVEN: &str = "https://maven.fabricmc.net/";
 
 #[async_trait]
 pub trait Loader {
-    async fn meta_struct<T>() -> T;
+    async fn download(&self) -> anyhow::Result<()>;
 
-    async fn download() -> anyhow::Result<()>;
+    fn create_json();
+
+    async fn dowload_file<P: AsRef<std::path::Path> + std::marker::Send>(
+        &self,
+        path: P,
+        url: String,
+    ) -> anyhow::Result<()> {
+        let path = path.as_ref();
+        let _ = std::fs::create_dir_all(path.parent().unwrap());
+
+        let mut file = std::fs::File::create(path)?;
+
+        let _response = tokio::task::spawn_blocking(move || -> anyhow::Result<()> {
+            reqwest::blocking::get(url)?.copy_to(&mut file)?;
+            Ok(())
+        })
+        .await??;
+        Ok(())
+    }
 }
