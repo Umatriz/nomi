035230d58bb1fa43160a4519df27b0d6307c5d33
Umatriz <artsharyi@gmail.com>
2023-07-10 07:46:19 +0300

    commit before pull

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
-pub mod utils;
-pub mod downloads;
 pub mod bootstrap;
-pub mod manifest;
 pub mod commands;
 pub mod configs;
+pub mod downloads;
 pub mod loaders;
+pub mod manifest;
+pub mod utils;
 
-use commands::{download_version};
+use commands::download_version;
+use loaders::{fabric::FabricLoader, maven::MavenUrl};
 
 slint::include_modules!();
 #[tokio::main]
 async fn main() {
-  let ui = MainWindow::new().unwrap();
-  ui.global::<State>().on_launch(|_id| {
-    tokio::spawn(download_version("id".to_string()));
-  });
-  ui.run().unwrap();
+    tokio::spawn(async {});
+    let ui = MainWindow::new().unwrap();
+    ui.global::<State>().on_launch(|_id| {});
+    ui.run().unwrap();
 }
