c5f5df300d7aedd6682189ddb92bd6b613aea473
Umatriz <artsharyi@gmail.com>
2023-07-11 18:49:13 +0300

    refactor: `MavenData`

--- src/loaders/maven.rs
+++ src/loaders/maven.rs
@@ -1,23 +1,37 @@
-pub struct MavenUrl(String, String);
+use std::path::PathBuf;
 
-impl MavenUrl {
-    pub fn build_url(maven: Vec<&str>) -> Self {
+pub struct MavenData {
+    pub url: String,
+    pub url_file: String,
+    pub local_file: String,
+    pub local_file_path: PathBuf,
+}
+
+impl MavenData {
+    pub fn new(maven_string: &str) -> Self {
+        let maven = Self::unwrap_maven(maven_string);
+
         let mut maven_url = String::new();
+        let mut local_path = PathBuf::new();
         maven.iter().for_each(|i| {
             maven_url.push('/');
+            local_path = local_path.join(i);
             maven_url.push_str(&urlencoding::encode(i))
         });
         let maven_file = maven.iter().rev().collect::<Vec<_>>();
-        MavenUrl(
-            maven_url,
-            format!(
-                "{}-{}",
+
+        Self {
+            url: maven_url,
+            url_file: format!(
+                "{}-{}.jar",
                 &urlencoding::encode(maven_file[1]),
                 &urlencoding::encode(maven_file[0])
             ),
-        )
+            local_file: format!("{}-{}.jar", maven_file[1], maven_file[0]),
+            local_file_path: local_path,
+        }
     }
-    pub fn unwrap_maven(maven: &str) -> Vec<&str> {
+    fn unwrap_maven(maven: &str) -> Vec<&str> {
         let splited = maven.split(':').rev().collect::<Vec<_>>();
         let first_element = splited[0];
         let res = splited
