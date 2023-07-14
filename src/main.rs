7f44a5a6fa44dd48950116511ad0560d891ec9c0
Umatriz <artsharyi@gmail.com>
2023-07-14 15:44:27 +0300

    `log4rs` config update

--- src/main.rs
+++ src/main.rs
@@ -8,46 +8,30 @@ pub mod utils;
 
 use commands::download_version;
 
-extern crate log;
-extern crate simplelog;
-
-use simplelog::*;
-
 use chrono::offset::Local;
+use log::info;
 use std::{fs::File, path::PathBuf};
+
+use crate::{downloads::Download, utils::GetPath};
 
 slint::include_modules!();
 #[tokio::main]
 async fn main() {
-    let time = Local::now().time().to_string().replace(':', ".");
-    let log_time = time.split('.').collect::<Vec<_>>();
-
-    let path = PathBuf::new().join("nomi_logs").join(format!(
-        "{}_{}.{}.{}.log",
-        Local::now().date_naive(),
-        log_time[0],
-        log_time[1],
-        log_time[2]
-    ));
-
-    let _ = std::fs::create_dir_all(path.parent().unwrap());
-
-    CombinedLogger::init(vec![
-        TermLogger::new(
-            LevelFilter::Info,
-            Config::default(),
-            TerminalMode::Mixed,
-            ColorChoice::Auto,
-        ),
-        WriteLogger::new(
-            LevelFilter::Info,
-            Config::default(),
-            File::create(path).unwrap(),
-        ),
-    ])
-    .unwrap();
+    log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
 
     info!("Start");
+
+    // tokio::spawn(async {
+    //     let version = Download::new().await;
+
+    //     version
+    //         .download(
+    //             "1.18.2".to_string(),
+    //             GetPath::game().to_string_lossy().to_string(),
+    //         )
+    //         .await
+    //         .unwrap();
+    // });
 
     let ui = MainWindow::new().unwrap();
     ui.global::<State>().on_launch(|_id| {
