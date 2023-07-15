24caecbf1e206145128b03f0777c927d6500337d
Umatriz <artsharyi@gmail.com>
2023-07-15 18:18:09 +0300

    `fern` date based setup

--- src/main.rs
+++ src/main.rs
@@ -8,34 +8,30 @@ pub mod utils;
 
 use commands::download_version;
 
-use chrono::offset::Local;
 use log::info;
-use std::{fs::File, path::PathBuf};
 
-use crate::{downloads::Download, utils::GetPath};
+use std::time::SystemTime;
+
+use crate::{
+    downloads::Download,
+    loaders::quilt::QuiltLoader,
+    utils::{logging::setup_logger, GetPath},
+};
 
 slint::include_modules!();
 #[tokio::main]
-async fn main() {
-    log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
+async fn main() -> anyhow::Result<()> {
+    setup_logger()?;
 
     info!("Start");
 
-    // tokio::spawn(async {
-    //     let version = Download::new().await;
-
-    //     version
-    //         .download(
-    //             "1.18.2".to_string(),
-    //             GetPath::game().to_string_lossy().to_string(),
-    //         )
-    //         .await
-    //         .unwrap();
-    // });
+    let _ = QuiltLoader::get_versions().await.unwrap();
 
     let ui = MainWindow::new().unwrap();
     ui.global::<State>().on_launch(|_id| {
         tokio::spawn(download_version("id".to_string()));
     });
     ui.run().unwrap();
+
+    Ok(())
 }
