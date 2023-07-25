513de0d1ab24d86c7322bde10f737d992e36471a
Umatriz <artsharyi@gmail.com>
2023-07-25 16:07:27 +0300

    basic `crates` and `xtask`

--- src-tauri/src/error.rs
+++
@@ -1,7 +0,0 @@
-use thiserror::Error;
-
-#[derive(Error, Debug)]
-pub enum Error {
-    #[error("Error: {0}")]
-    General(String),
-}
--- src-tauri/src/prelude.rs
+++
@@ -1,6 +0,0 @@
-pub use crate::error::Error;
-
-pub type Result<T> = core::result::Result<T, Error>;
-
-/// Wrapper for newtype pattern
-pub struct W<T>(pub T);
