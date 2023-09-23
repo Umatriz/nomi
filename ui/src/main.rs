a63edfc50cc7ce19b23bee6e0a9ed73e1207c594
Umatriz <artsharyi@gmail.com>
2023-09-23 09:29:44 +0300

    feta: tarcing setup

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -2,15 +2,32 @@
 use dioxus::prelude::*;
 use nomi_core::{downloads::version::DownloadVersion, loaders::vanilla::Vanilla};
 use tracing::Level;
+use tracing_subscriber::{
+    fmt::{writer::MakeWriterExt, Layer},
+    prelude::__tracing_subscriber_SubscriberExt,
+    Registry,
+};
 
 pub mod components;
 
 fn main() {
-    let subscriber = tracing_subscriber::fmt()
-        .with_max_level(Level::DEBUG)
-        .pretty()
-        .finish();
-    tracing::subscriber::set_global_default(subscriber).unwrap();
+    let file_appender = tracing_appender::rolling::hourly("./logs", "nomi.log");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+
+    let registry = Registry::default()
+        .with(
+            Layer::default()
+                .with_writer(non_blocking.with_max_level(Level::INFO))
+                .with_ansi(false)
+                .compact(),
+        )
+        .with(
+            Layer::default()
+                .with_writer(std::io::stdout.with_max_level(Level::DEBUG))
+                .pretty(),
+        );
+
+    tracing::subscriber::set_global_default(registry).unwrap();
 
     dioxus_desktop::launch(App);
 }
