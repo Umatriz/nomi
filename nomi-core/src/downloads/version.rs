61c7a52d03d4b47284555675c4a074ef482bf1c7
Umatriz <artsharyi@gmail.com>
2023-09-29 16:26:00 +0300

    feat: add `Fabric` `new` function

--- nomi-core/src/downloads/version.rs
+++ nomi-core/src/downloads/version.rs
@@ -4,9 +4,9 @@ use async_trait::async_trait;
 
 #[async_trait(?Send)]
 pub trait DownloadVersion {
-    async fn download<P: AsRef<Path>>(&self, dir: P) -> anyhow::Result<()>;
+    async fn download(&self, dir: impl AsRef<Path>) -> anyhow::Result<()>;
 
-    async fn download_libraries<P: AsRef<Path>>(&self, dir: P) -> anyhow::Result<()>;
+    async fn download_libraries(&self, dir: impl AsRef<Path>) -> anyhow::Result<()>;
 
-    async fn create_json<P: AsRef<Path>>(&self, dir: P) -> anyhow::Result<()>;
+    async fn create_json(&self, dir: impl AsRef<Path>) -> anyhow::Result<()>;
 }
--- nomi-core/src/loaders/fabric.rs
+++ nomi-core/src/loaders/fabric.rs
@@ -16,10 +16,9 @@ pub struct Fabric {
 impl Fabric {
     pub async fn new(
         game_version: impl Into<String>,
-        loader_version: impl Into<String>,
+        loader_version: Option<impl Into<String>>,
     ) -> anyhow::Result<Self> {
         let game_version = game_version.into();
-        let loader_version = loader_version.into();
 
         let client = Client::new();
         let launcher_manifest = get_launcher_manifest().await?;
@@ -30,26 +29,53 @@ impl Fabric {
         {
             return Err(crate::error::Error::NoSuchVersion.into());
         };
-        let versions = client.get("");
 
-        Ok(Self {
-            versions: todo!(),
-            profile: todo!(),
-        })
+        let versions: FabricVersions = client
+            .get(format!(
+                "https://meta.fabricmc.net/v2/versions/loader/{}",
+                game_version
+            ))
+            .send()
+            .await?
+            .json()
+            .await?;
+
+        let profile_version = if let Some(loader) = loader_version {
+            let loader = loader.into();
+            if let Some(loader) = versions.iter().find(|i| i.loader.version == loader) {
+                loader
+            } else {
+                &versions[0]
+            }
+        } else {
+            &versions[0]
+        };
+
+        let profile: FabricProfile = client
+            .get(format!(
+                "https://meta.fabricmc.net/v2/versions/loader/{}/{}/profile/json",
+                game_version, profile_version.loader.version
+            ))
+            .send()
+            .await?
+            .json()
+            .await?;
+
+        Ok(Self { versions, profile })
     }
 }
 
 #[async_trait(?Send)]
 impl DownloadVersion for Fabric {
-    async fn download<P: AsRef<Path>>(&self, dir: P) -> anyhow::Result<()> {
+    async fn download(&self, dir: impl AsRef<Path>) -> anyhow::Result<()> {
         Ok(())
     }
 
-    async fn download_libraries<P: AsRef<Path>>(&self, dir: P) -> anyhow::Result<()> {
+    async fn download_libraries(&self, dir: impl AsRef<Path>) -> anyhow::Result<()> {
         Ok(())
     }
 
-    async fn create_json<P: AsRef<Path>>(&self, dir: P) -> anyhow::Result<()> {
+    async fn create_json(&self, dir: impl AsRef<Path>) -> anyhow::Result<()> {
         Ok(())
     }
 }
