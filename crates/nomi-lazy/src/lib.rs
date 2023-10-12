eb7ff91620f7415c9d7a6909e250e4606f840e92
Umatriz <artsharyi@gmail.com>
2023-10-12 15:30:47 +0300

    fix: `Lazy::new_try` type error fix

--- crates/nomi-lazy/src/lib.rs
+++ crates/nomi-lazy/src/lib.rs
@@ -8,10 +8,24 @@ pub struct Undefined;
 pub struct NotTry;
 pub struct Try;
 
-pub struct LazyBuilder;
+#[derive(Debug)]
+pub struct Lazy<
+    T,
+    State = Undefined,
+    Out = T,
+    F = fn() -> LazyFutureAlias<Out>,
+    Fut = LazyFutureAlias<Out>,
+> where
+    F: Fn() -> Fut,
+    Fut: Future<Output = Out>,
+{
+    cell: OnceCell<T>,
+    init: F,
+    _marker: PhantomData<State>,
+}
 
-impl LazyBuilder {
-    pub const fn new_not_try<T, F, Fut>(init: F) -> Lazy<T, NotTry, F, Fut>
+impl<T> Lazy<T> {
+    pub const fn new_not_try<F, Fut>(init: F) -> Lazy<T, NotTry, T, F, Fut>
     where
         F: Fn() -> Fut,
         Fut: Future<Output = T>,
@@ -23,10 +37,10 @@ impl LazyBuilder {
         }
     }
 
-    pub const fn new_try<T, F, Fut>(init: F) -> Lazy<T, Try, F, Fut, anyhow::Result<T>>
+    pub const fn new_try<F, Fut, Out>(init: F) -> Lazy<T, Try, Out, F, Fut>
     where
         F: Fn() -> Fut,
-        Fut: Future<Output = anyhow::Result<T>>,
+        Fut: Future<Output = Out>,
     {
         Lazy {
             cell: OnceCell::const_new(),
@@ -36,18 +50,7 @@ impl LazyBuilder {
     }
 }
 
-#[derive(Debug)]
-pub struct Lazy<T, State, F = fn() -> LazyFutureAlias<T>, Fut = LazyFutureAlias<T>, Out = T>
-where
-    F: Fn() -> Fut,
-    Fut: Future<Output = Out>,
-{
-    cell: OnceCell<T>,
-    init: F,
-    _marker: PhantomData<State>,
-}
-
-impl<T, F, Fut> Lazy<T, NotTry, F, Fut>
+impl<T, F, Fut> Lazy<T, NotTry, T, F, Fut>
 where
     F: Fn() -> Fut,
     Fut: Future<Output = T>,
@@ -61,7 +64,7 @@ where
     }
 }
 
-impl<T, F, Fut> Lazy<T, Try, F, Fut, anyhow::Result<T>>
+impl<T, F, Fut> Lazy<T, Try, anyhow::Result<T>, F, Fut>
 where
     F: Fn() -> Fut,
     Fut: Future<Output = anyhow::Result<T>>,
@@ -79,17 +82,27 @@ where
 mod tests {
     use super::*;
 
-    static STATE: Lazy<i32, NotTry> = LazyBuilder::new_not_try(|| {
+    static STATIC_STATE: Lazy<i32, NotTry> = Lazy::new_not_try(|| {
         Box::pin(async {
             let _ = tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
             1
         })
     });
 
+    static TRY_STATE: Lazy<i32, Try, anyhow::Result<i32>> =
+        Lazy::new_try(|| Box::pin(async { Ok(1) }));
+
     #[tokio::test]
-    async fn init_test() {
-        let t = STATE.get_or_init().await;
+    async fn static_test() {
+        let t = STATIC_STATE.get_or_init().await;
 
         assert_eq!(t, &1 as &i32);
+    }
+
+    #[tokio::test]
+    async fn try_test() {
+        let t = TRY_STATE.get_or_init().await;
+
+        assert_eq!(t.unwrap(), &1 as &i32);
     }
 }
