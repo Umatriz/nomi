b2c4c023237f97f6e1e697961cac1c6d06277b43
OlshaMB <michaelolshanskiy@icloud.com>
2024-05-11 10:24:06 +0300

    Code style changes in default_pixels_per_point_value function

--- crates/client/src/client_settings.rs
+++ crates/client/src/client_settings.rs
@@ -1,17 +1,19 @@
 #[derive(serde::Serialize, serde::Deserialize, Clone, Copy)]
 pub struct ClientSettings {
-    pub pixels_per_point: Option<f32>
+    pub pixels_per_point: Option<f32>,
 }
 // Maybe use somekind library to detect this?
 pub fn default_pixels_per_point_value() -> f32 {
-    #[cfg(not(target_os = "macos"))]
-    return 1.2;
-    // We assume that display dpi on macos is higher, although it can be not true
-    #[cfg(target_os = "macos")]
-    2.0
+    if cfg!(target_os = "macos") {
+        2.0
+    } else {
+        1.2
+    }
 }
 impl Default for ClientSettings {
     fn default() -> Self {
-        Self { pixels_per_point: Some(default_pixels_per_point_value()) }
+        Self {
+            pixels_per_point: Some(default_pixels_per_point_value()),
+        }
     }
 }
